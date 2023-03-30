aee6ef4b3999be833bf5cd13f8128e90308d39c8
PC07_CDC3 <appdata.123@gmail.com>
2023-03-29 18:53:00 -0600

    Practica 1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+    }
+}
